bcce84f5ca683c19b94e2b2cca486da2f2dc5007
bmuehleis <benediktmuehleis@gmail.com>
2026-04-16 16:35:48 +0200

    #Project restart: - restructured - start from beginning - new idea and app stack

---
+++ src/app.py
@@ -0,0 +1,9 @@
+# Streamlit UI
+
+import streamlit as st
+import plotly.express as px
+
+st.write("""
+         # My first app
+         Hello *world!*
+         """)
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,18 +1,5 @@
+# handle data loading
+
 import pandas as pd
-import os
 
-def load_price_data(folder_path):
-    data = {}
-
-    for file in os.listdir(folder_path):
-        if file.endswith(".csv"):
-            name = file.replace(".csv", "")
-            df = pd.read_csv(os.path.join(folder_path, file))
-
-            df['Date'] = pd.to_datetime(df['Date'])
-            df = df.sort_values('Date')
-
-            data[name] = df.set_index('Date')['Price']
-
-    combined = pd.DataFrame(data)
-    return combined
+data = pd.read_csv('data.csv')
