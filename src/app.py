1126dbc84aac526ac48f0ee73a373cc9be72a8f8
bmuehleis <benediktmuehleis@gmail.com>
2026-04-20 12:13:23 +0200

    moved init to app.py

--- src/app.py
+++ src/app.py
@@ -2,7 +2,13 @@
 
 import streamlit as st
 import plotly.express as px
+from data_loader import init_dataframe, returns_df
 
+#Load csv
+init_dataframe()
+print(returns_df)
+
+#Streamlit
 st.write("""
          # My first app
          Hello *world!*
